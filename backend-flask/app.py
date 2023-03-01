2ab4a9c688fb0460ff74750afe74cde9c6d2f2c1
Andrew Douglas <fairtomiddlingster@gmail.com>
2023-03-01 15:46:39 +0000

    Added rollbar stuff, but not the homework I don't think.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -32,6 +32,11 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar specific
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # Configuring Logger to Use CloudWatch
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
@@ -80,6 +85,29 @@ def after_request(response):
     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
 
+# Rollbar
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        os.getenv("ROLLBAR_ACCESS_TOKEN"),
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route("/rollbar/test")
+def rollbar_test():
+  rollbar.report_message('Hello World!','warning')
+  return "Hello World!"
+
+# Normal stuff
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
   user_handle  = 'andrewbrown'
